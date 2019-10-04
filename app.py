6911b646345bd1cafa55675d8786850710477021
ablades <audaris@gmail.com>
2019-10-03 19:31:32 -0700

    add filter function

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
-
 from dotenv import load_dotenv
-load_dotenv()
-
 from flask import Flask, render_template, request
 import requests
 import json
+
+load_dotenv()
+
 app = Flask(__name__)
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
 #Show initial search page with attributions and big search bar
@@ -26,11 +26,19 @@ def index():
     }
     r = requests.get("https://api.themoviedb.org/3/search/multi?", query_string)
     movies = json.loads(r.content)["results"]
-    print(movies)
-    return render_template('index.html', movies = movies)
 
+    #Thanks to Mr. Ben Lafferty for 
+    filtered_list = list(filter(lambda x: (x.get("title") or x.get("original_title" or x.get("original_name"))) and x.get("poster_path"), movies))
+
+    #Check if movie has a title or a name remove it if it does not have either
+    # movies_list = filter(lambda movie: (movie.get("title", False) or movie.get("original_name", False)) and movie.get("poster_path", False), movies)
+    # movies_list = list(filter(lambda x: (x.get("title", False)) or (x.get("original_name", False)) and (x.get("poster_path", False)), movies))
+    print(filtered_list)
+        
+    return render_template('index.html', movies = filtered_list)
+
+#Show shopping cart
 @app.route('/cart')
 def cart():
     """Show Cart with cart contents""" 
     return render_template('cart.html')
-    pass
